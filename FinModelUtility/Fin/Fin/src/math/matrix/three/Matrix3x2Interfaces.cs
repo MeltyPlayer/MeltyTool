e194a544c6930be672a83c0e2653be945c1efee6
MeltyPlayer <meltyplayer@gmail.com>
2024-04-17 05:18:23 -0500

    Generated auto readonly interfaces for the matrix types.

--- FinModelUtility/Fin/Fin/src/math/matrix/three/Matrix3x2Interfaces.cs
+++ FinModelUtility/Fin/Fin/src/math/matrix/three/Matrix3x2Interfaces.cs
@@ -1,17 +1,24 @@
 ﻿using System.Numerics;
 
-namespace fin.math.matrix.three {
-  public interface IFinMatrix3x2
-      : IFinMatrix<IFinMatrix3x2, IReadOnlyFinMatrix3x2, Matrix3x2>,
-        IReadOnlyFinMatrix3x2 { }
+using schema.readOnly;
 
-  public interface IReadOnlyFinMatrix3x2
-      : IReadOnlyFinMatrix<IFinMatrix3x2, IReadOnlyFinMatrix3x2, Matrix3x2> {
+namespace fin.math.matrix.three {
+  [GenerateReadOnly]
+  public partial interface IFinMatrix3x2
+      : IFinMatrix<IFinMatrix3x2, IReadOnlyFinMatrix3x2, Matrix3x2> {
+    [Const]
     void CopyTranslationInto(out Vector2 dst);
+
+    [Const]
     void CopyRotationInto(out float dst);
+
+    [Const]
     void CopyScaleInto(out Vector2 dst);
+
+    [Const]
     void CopySkewXRadiansInto(out float dst);
 
+    [Const]
     void Decompose(out Vector2 translation,
                    out float rotation,
                    out Vector2 scale,
