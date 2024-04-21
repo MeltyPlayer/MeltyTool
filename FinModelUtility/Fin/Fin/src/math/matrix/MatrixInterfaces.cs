5476ddcf7c319930258c521ca77f253448259810
MeltyPlayer <meltyplayer@gmail.com>
2024-04-21 04:47:55 -0500

    Revert "Converted the base matrix type to the readonly generator."
    This reverts commit d6791e8ade67dbdaa29caf1ef63564ad2ddd7c5e.

--- FinModelUtility/Fin/Fin/src/math/matrix/MatrixInterfaces.cs
+++ FinModelUtility/Fin/Fin/src/math/matrix/MatrixInterfaces.cs
@@ -1,14 +1,14 @@
-﻿using schema.readOnly;
+﻿using System;
 
 namespace fin.math.matrix {
   // The type parameters on these matrices are kind of janky, but they allow us
   // to have consistent interfaces between 3x3 and 4x4 matrices.
 
-  [GenerateReadOnly]
-  public partial interface IFinMatrix<TMutable, TReadOnly, TImpl>
+  public interface IFinMatrix<TMutable, TReadOnly, TImpl>
+      : IReadOnlyFinMatrix<TMutable, TReadOnly, TImpl>
       where TMutable : IFinMatrix<TMutable, TReadOnly, TImpl>, TReadOnly
       where TReadOnly : IReadOnlyFinMatrix<TMutable, TReadOnly, TImpl> {
-    TImpl Impl { get; set; }
+    new TImpl Impl { get; set; }
 
     void CopyFrom(TReadOnly other);
     void CopyFrom(in TImpl other);
@@ -16,7 +16,7 @@ namespace fin.math.matrix {
     TMutable SetIdentity();
     TMutable SetZero();
 
-    float this[int row, int column] { get; set; }
+    new float this[int row, int column] { get; set; }
 
     TMutable AddInPlace(TReadOnly other);
     TMutable AddInPlace(in TImpl other);
@@ -25,38 +25,34 @@ namespace fin.math.matrix {
     TMutable MultiplyInPlace(float other);
 
     TMutable InvertInPlace();
+  }
 
-    [Const]
+  public interface IReadOnlyFinMatrix<TMutable, TReadOnly, TImpl>
+      : IEquatable<TReadOnly>
+      where TMutable : IFinMatrix<TMutable, TReadOnly, TImpl>, TReadOnly
+      where TReadOnly : IReadOnlyFinMatrix<TMutable, TReadOnly, TImpl> {
+    TImpl Impl { get; }
+
     TMutable Clone();
 
-    [Const]
+    float this[int row, int column] { get; }
+
     TMutable CloneAndAdd(TReadOnly other);
-    [Const]
     void AddIntoBuffer(TReadOnly other, TMutable buffer);
 
-    [Const]
     TMutable CloneAndMultiply(TReadOnly other);
-    [Const]
     void MultiplyIntoBuffer(TReadOnly other, TMutable buffer);
 
-    [Const]
     TMutable CloneAndAdd(in TImpl other);
-    [Const]
     void AddIntoBuffer(in TImpl other, TMutable buffer);
 
-    [Const]
     TMutable CloneAndMultiply(in TImpl other);
-    [Const]
     void MultiplyIntoBuffer(in TImpl other, TMutable buffer);
 
-    [Const]
     TMutable CloneAndMultiply(float other);
-    [Const]
     void MultiplyIntoBuffer(float other, TMutable buffer);
 
-    [Const]
     TMutable CloneAndInvert();
-    [Const]
     void InvertIntoBuffer(TMutable buffer);
   }
 }
