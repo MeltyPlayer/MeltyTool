3d946c8a9e0dfe2724a16ec0fb3e8a55baa28f24
MeltyPlayer <meltyplayer@gmail.com>
2024-04-25 01:08:19 -0500

    Updated matrix definition to use readonly.

--- FinModelUtility/Fin/Fin/src/math/matrix/four/Matrix4x4Interfaces.cs
+++ FinModelUtility/Fin/Fin/src/math/matrix/four/Matrix4x4Interfaces.cs
@@ -2,22 +2,30 @@
 
 using fin.model;
 
-namespace fin.math.matrix.four {
-  public interface IFinMatrix4x4
-      : IFinMatrix<IFinMatrix4x4, IReadOnlyFinMatrix4x4, Matrix4x4>,
-        IReadOnlyFinMatrix4x4 {
-    IFinMatrix4x4 TransposeInPlace();
-  }
+using schema.readOnly;
 
-  public interface IReadOnlyFinMatrix4x4
-      : IReadOnlyFinMatrix<IFinMatrix4x4, IReadOnlyFinMatrix4x4, Matrix4x4> {
+namespace fin.math.matrix.four {
+  [GenerateReadOnly]
+  public partial interface IFinMatrix4x4
+      : IFinMatrix<IFinMatrix4x4, IReadOnlyFinMatrix4x4, Matrix4x4> {
+    IFinMatrix4x4 TransposeInPlace();
+
+    [Const]
     IFinMatrix4x4 CloneAndTranspose();
+
+    [Const]
     void TransposeIntoBuffer(IFinMatrix4x4 buffer);
 
+    [Const]
     void CopyTranslationInto(out Position dst);
+
+    [Const]
     void CopyRotationInto(out Quaternion dst);
+
+    [Const]
     void CopyScaleInto(out Scale dst);
 
+    [Const]
     void Decompose(out Position translation,
                    out Quaternion rotation,
                    out Scale scale);
