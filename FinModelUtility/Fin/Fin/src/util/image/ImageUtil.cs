f3b7fcae3c6d1cf7a532d4fa08d43f1c9b307231
MeltyPlayer <meltyplayer@gmail.com>
2024-05-05 22:31:21 -0500

    Slightly optimized how we detect the transparency type of textures.

--- FinModelUtility/Fin/Fin/src/util/image/ImageUtil.cs
+++ FinModelUtility/Fin/Fin/src/util/image/ImageUtil.cs
@@ -1,4 +1,5 @@
 ﻿using fin.image;
+using fin.image.formats;
 
 namespace fin.util.image {
   public enum ImageTransparencyType {
@@ -14,27 +15,68 @@ namespace fin.util.image {
         return ImageTransparencyType.OPAQUE;
       }
 
-      var transparencyType = ImageTransparencyType.OPAQUE;
-      image.Access(
-          getHandler => {
-            for (var y = 0; y < image.Height; ++y) {
-              for (var x = 0; x < image.Width; ++x) {
-                getHandler(x, y, out _, out _, out _, out var a);
-                switch (a) {
-                  case 0: {
-                    transparencyType = ImageTransparencyType.MASK;
-                    break;
-                  }
-                  case < 255: {
-                    transparencyType = ImageTransparencyType.TRANSPARENT;
-                    return;
+      switch (image) {
+        case La16Image la16Image: {
+          using var imgLock = la16Image.Lock();
+
+          var transparencyType = ImageTransparencyType.OPAQUE;
+          foreach (var pixel in imgLock.Pixels) {
+            switch (pixel.A) {
+              case 0: {
+                transparencyType = ImageTransparencyType.MASK;
+                break;
+              }
+              case < 255: {
+                return ImageTransparencyType.TRANSPARENT;
+              }
+            }
+          }
+
+          return transparencyType;
+        }
+        case Rgba32Image rgba32Image: {
+          using var imgLock = rgba32Image.Lock();
+
+          var transparencyType = ImageTransparencyType.OPAQUE;
+          foreach (var pixel in imgLock.Pixels) {
+            switch (pixel.A) {
+              case 0: {
+                transparencyType = ImageTransparencyType.MASK;
+                break;
+              }
+              case < 255: {
+                return ImageTransparencyType.TRANSPARENT;
+              }
+            }
+          }
+
+          return transparencyType;
+        }
+      }
+
+      {
+        var transparencyType = ImageTransparencyType.OPAQUE;
+        image.Access(
+            getHandler => {
+              for (var y = 0; y < image.Height; ++y) {
+                for (var x = 0; x < image.Width; ++x) {
+                  getHandler(x, y, out _, out _, out _, out var a);
+                  switch (a) {
+                    case 0: {
+                      transparencyType = ImageTransparencyType.MASK;
+                      break;
+                    }
+                    case < 255: {
+                      transparencyType = ImageTransparencyType.TRANSPARENT;
+                      return;
+                    }
                   }
                 }
               }
-            }
-          });
+            });
 
-      return transparencyType;
+        return transparencyType;
+      }
     }
   }
 }
