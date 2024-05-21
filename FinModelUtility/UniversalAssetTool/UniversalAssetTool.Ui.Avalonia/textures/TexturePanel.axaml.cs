2567e13ebc290c154df757ec12b11a82d28f27a7
MeltyPlayer <meltyplayer@gmail.com>
2024-05-21 16:51:37 -0500

    Set up basic heading classes similar to HTML and wrapped the material panel in a GroupBox.

---
+++ FinModelUtility/UniversalAssetTool/UniversalAssetTool.Ui.Avalonia/textures/TexturePanel.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace uni.ui.avalonia.textures {
+  public partial class TexturePanel : UserControl {
+    public TexturePanel() {
+      InitializeComponent();
+    }
+  }
+}
