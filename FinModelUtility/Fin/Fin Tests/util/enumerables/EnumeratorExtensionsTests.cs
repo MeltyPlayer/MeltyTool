93d1d645d3385ebd172b24d85b400013a905748d
MeltyPlayer <meltyplayer@gmail.com>
2024-06-09 09:43:39 -0500

    Set up a helper method for trying to move an enumerator.

---
+++ FinModelUtility/Fin/Fin Tests/util/enumerables/EnumeratorExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+using fin.util.asserts;
+
+using NUnit.Framework;
+
+namespace fin.util.enumerables {
+  public class EnumeratorExtensionsTests {
+    [Test]
+    public void TestTryMoveNextEmpty() {
+      var emptyEnumerator = Enumerable.Empty<int>().ToEnumerator();
+
+      Asserts.False(emptyEnumerator.TryMoveNext(out _));
+      Asserts.False(emptyEnumerator.TryMoveNext(out _));
+      Asserts.False(emptyEnumerator.TryMoveNext(out _));
+    }
+
+    [Test]
+    public void TestTryMoveNext() {
+      var enumerator = new[] { 1, 2, 3 }.ToEnumerator();
+
+      Asserts.True(enumerator.TryMoveNext(out var first));
+      Asserts.Equal(1, first);
+
+      Asserts.True(enumerator.TryMoveNext(out var second));
+      Asserts.Equal(2, second);
+
+      Asserts.True(enumerator.TryMoveNext(out var third));
+      Asserts.Equal(3, third);
+
+      Asserts.False(enumerator.TryMoveNext(out _));
+      Asserts.False(enumerator.TryMoveNext(out _));
+      Asserts.False(enumerator.TryMoveNext(out _));
+    }
+  }
+}
