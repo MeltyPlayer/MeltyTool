4bb5d26cae3e632d43e7319bdf4c567bf0b94e24
MeltyPlayer <meltyplayer@gmail.com>
2024-06-23 19:48:36 -0500

    Updated TTYD matrix logic based on what I found from decomp.

--- FinModelUtility/Games/PaperMarioTheThousandYearDoor/PaperMarioTheThousandYearDoor/src/api/TtydGroupTransformUtils.cs
+++ FinModelUtility/Games/PaperMarioTheThousandYearDoor/PaperMarioTheThousandYearDoor/src/api/TtydGroupTransformUtils.cs
@@ -16,66 +16,80 @@ namespace ttyd.api {
 
       var translation = new Vector3(groupTransforms.Slice(0, 3));
       var scale = new Vector3(groupTransforms.Slice(3, 3));
-      var rotationIn2Degrees
+      var rotationDegrees1
           = new Vector3(groupTransforms.Slice(6, 3));
-      var jointPostRotationInDegrees
-          = new Vector3(groupTransforms.Slice(9, 3));
-      var rotationCenter
-          = new Vector3(groupTransforms.Slice(12, 3));
-      var scaleCenter
-          = new Vector3(groupTransforms.Slice(15, 3));
-      var rotationPivot
-          = new Vector3(groupTransforms.Slice(18, 3));
-      var scalePivot
-          = new Vector3(groupTransforms.Slice(21, 3));
 
       Func<Matrix4x4, Matrix4x4, Matrix4x4> combineMatrices
           = (lhs, rhs) => rhs * lhs;
 
-      var translationMatrix = Matrix4x4.CreateTranslation(translation);
-
-      var scaleMatrix = Matrix4x4.CreateTranslation(scaleCenter + scalePivot);
-      scaleMatrix = combineMatrices(scaleMatrix, Matrix4x4.CreateScale(scale));
-      scaleMatrix = combineMatrices(scaleMatrix,
-                                    Matrix4x4.CreateTranslation(-scaleCenter));
-
       var deg2Rad = MathF.PI / 180;
-      var rotationMatrix
-          = Matrix4x4.CreateTranslation(rotationCenter + rotationPivot);
-      rotationMatrix = combineMatrices(
-          rotationMatrix,
-          SystemMatrix4x4Util.FromRotation(
-              QuaternionUtil.CreateZyxRadians(
-                  jointPostRotationInDegrees * deg2Rad)));
-      rotationMatrix = combineMatrices(
-          rotationMatrix,
-          SystemMatrix4x4Util.FromRotation(
-              QuaternionUtil.CreateZyxRadians(
-                  rotationIn2Degrees * 2 * deg2Rad)));
-      rotationMatrix = combineMatrices(
-          rotationMatrix,
-          Matrix4x4.CreateTranslation(-rotationCenter));
+      if (ttydGroup.IsJoint) {
+        var rotationDegrees2
+            = new Vector3(groupTransforms.Slice(9, 3));
 
-      if (ttydGroupToParent.TryGetValue(ttydGroup, out var parentTtydGroup)) {
-        var parentScale
-            = new Vector3(allGroupTransforms.Slice(
-                              parentTtydGroup.TransformBaseIndex + 3,
-                              3));
-        var invertedParentScale
-            = new Vector3(1 / parentScale.X,
-                          1 / parentScale.Y,
-                          1 / parentScale.Z);
+        var jointMatrix = Matrix4x4.CreateTranslation(translation);
 
-        rotationMatrix
-            = combineMatrices(Matrix4x4.CreateScale(invertedParentScale),
-                              rotationMatrix);
+        if (ttydGroupToParent.TryGetValue(ttydGroup, out var parentTtydGroup)) {
+          var parentScale
+              = new Vector3(allGroupTransforms.Slice(
+                                parentTtydGroup.TransformBaseIndex + 3,
+                                3));
+          jointMatrix = combineMatrices(
+              jointMatrix,
+              Matrix4x4.CreateScale(new Vector3(1 / parentScale.X,
+                                                1 / parentScale.Y,
+                                                1 / parentScale.Z)));
+        }
+
+        jointMatrix
+            = combineMatrices(jointMatrix, Matrix4x4.CreateScale(scale));
+        jointMatrix = combineMatrices(jointMatrix,
+                                      SystemMatrix4x4Util.FromRotation(
+                                          QuaternionUtil.CreateZyxRadians(
+                                              rotationDegrees2 *
+                                              deg2Rad)));
+        jointMatrix = combineMatrices(jointMatrix,
+                                      SystemMatrix4x4Util.FromRotation(
+                                          QuaternionUtil.CreateZyxRadians(
+                                              rotationDegrees1 *
+                                              deg2Rad *
+                                              2)));
+        return jointMatrix;
       }
 
-      var matrix = translationMatrix;
-      matrix = combineMatrices(matrix, rotationMatrix);
-      matrix = combineMatrices(matrix, scaleMatrix);
+      var rotationCenter
+          = new Vector3(groupTransforms.Slice(12, 3));
+      var scaleCenter
+          = new Vector3(groupTransforms.Slice(15, 3));
+      var rotationTranslation
+          = new Vector3(groupTransforms.Slice(18, 3));
+      var scaleTranslation
+          = new Vector3(groupTransforms.Slice(21, 3));
 
-      return matrix;
+      var nonJointMatrix = Matrix4x4.CreateTranslation(translation);
+
+      nonJointMatrix = combineMatrices(
+          nonJointMatrix,
+          Matrix4x4.CreateTranslation(scaleCenter + scaleTranslation));
+      nonJointMatrix
+          = combineMatrices(nonJointMatrix, Matrix4x4.CreateScale(scale));
+      nonJointMatrix
+          = combineMatrices(nonJointMatrix,
+                            Matrix4x4.CreateTranslation(-scaleCenter));
+
+      nonJointMatrix = combineMatrices(
+          nonJointMatrix,
+          Matrix4x4.CreateTranslation(rotationCenter + rotationTranslation));
+      nonJointMatrix
+          = combineMatrices(nonJointMatrix,
+                            SystemMatrix4x4Util.FromRotation(
+                                QuaternionUtil.CreateZyxRadians(
+                                    rotationDegrees1 * 2 * deg2Rad)));
+      nonJointMatrix
+          = combineMatrices(nonJointMatrix,
+                            Matrix4x4.CreateTranslation(-rotationCenter));
+
+      return nonJointMatrix;
     }
   }
 }
