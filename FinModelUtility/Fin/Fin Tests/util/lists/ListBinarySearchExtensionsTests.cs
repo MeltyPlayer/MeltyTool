498d9d7d6d054448296955c83a21dbad3b285a75
MeltyPlayer <meltyplayer@gmail.com>
2024-06-26 02:10:19 -0500

    Set up helper logic for doing a binary search without a value.

---
+++ FinModelUtility/Fin/Fin Tests/util/lists/ListBinarySearchExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace fin.util.lists;
+
+public class ListBinarySearchExtensionsTests {
+  public record A(int Value);
+
+  public readonly struct AStaticComparer(int value)
+      : IStaticAsymmetricComparer<A, int> {
+    public int Value => value;
+    public int Compare(A lhs, int rhs) => lhs.Value.CompareTo(rhs);
+  }
+
+  [Test]
+  public void TestEasyBinarySearch() {
+    var values = new List<A> { new(0), new(1), new(2) };
+
+    var index = values.BinarySearch<A, int, AStaticComparer>(
+        new AStaticComparer(1));
+
+    Assert.AreEqual(1, index);
+  }
+
+  [Test]
+  public void TestMissingBinarySearch() {
+    var values = new List<A> { new(0), new(2) };
+
+    var index = values.BinarySearch<A, int, AStaticComparer>(
+        new AStaticComparer(1));
+
+    Assert.AreEqual(~1, index);
+  }
+
+  [Test]
+  public void TestTooLargeBinarySearch() {
+    var values = new List<A> { new(0), new(2) };
+
+    var index = values.BinarySearch<A, int, AStaticComparer>(
+        new AStaticComparer(3));
+
+    Assert.AreEqual(~2, index);
+  }
+}
