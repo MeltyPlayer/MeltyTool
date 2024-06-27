b271203d53ec437cca18d0ef12d6b32c6cec22f9
MeltyPlayer <meltyplayer@gmail.com>
2024-06-27 01:42:59 -0500

    Set up tests for TTYD.

---
+++ FinModelUtility/PaperMarioTheThousandYearDoor/PaperMarioTheThousandYearDoorTests/TtydModelGoldenTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+using fin.io;
+using fin.testing.model;
+
+using fin.testing;
+
+using ttyd.api;
+
+namespace ttyd;
+
+public class TtydModelGoldenTests
+    : BModelGoldenTests<TtydModelFileBundle, TtydModelImporter> {
+  [Test]
+  [TestCaseSource(nameof(GetGoldenDirectories_))]
+  public void TestExportsGoldenAsExpected(
+      IFileHierarchyDirectory goldenDirectory)
+    => this.AssertGolden(goldenDirectory);
+
+  public override TtydModelFileBundle GetFileBundleFromDirectory(
+      IFileHierarchyDirectory directory)
+    => new() {
+        ModelFile = directory.GetExistingFiles()
+                             .Single(f => !f.Name.EndsWith('-'))
+    };
+
+  private static IFileHierarchyDirectory[] GetGoldenDirectories_()
+    => GoldenAssert
+       .GetGoldenDirectories(
+           GoldenAssert
+               .GetRootGoldensDirectory(Assembly.GetExecutingAssembly()))
+       .SelectMany(dir => dir.GetExistingSubdirs())
+       .ToArray();
+}
