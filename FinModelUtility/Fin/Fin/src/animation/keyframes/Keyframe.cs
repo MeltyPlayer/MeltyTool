4cecfeb389d8fb73b33ab4aa98e89dfe2f90112c
MeltyPlayer <meltyplayer@gmail.com>
2024-06-26 20:13:04 -0500

    Added the new Keyframe types.

---
+++ FinModelUtility/Fin/Fin/src/animation/keyframes/Keyframe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace fin.animation.keyframes;
+
+public readonly record struct Keyframe<T>(
+    float Frame,
+    T Value,
+    string FrameType = "")
+    : IKeyframe<T>,
+      IComparable<Keyframe<T>> {
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public int CompareTo(Keyframe<T> other)
+    => FrameComparisonUtil.CompareFrames(this.Frame, other.Frame);
+
+  public bool Equals(KeyframeDefinition<T> other)
+    => FrameComparisonUtil.AreSameFrame(this.Frame, other.Frame) &&
+       (this.Value?.Equals(other.Value) ??
+        this.Value == null && other.Value == null);
+}
---
+++ FinModelUtility/Fin/Fin/src/animation/keyframes/KeyframeWithTangents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace fin.animation.keyframes;
+
+public readonly record struct KeyframeWithTangents<T>(
+    float Frame,
+    T Value,
+    float TangentIn,
+    float TangentOut)
+    : IKeyframeWithTangents<T>, IComparable<KeyframeWithTangents<T>> {
+  public KeyframeWithTangents(float frame, T value, float tangent) : this(
+      frame,
+      value,
+      tangent,
+      tangent) { }
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public int CompareTo(KeyframeWithTangents<T> other)
+    => FrameComparisonUtil.CompareFrames(this.Frame, other.Frame);
+
+  public bool Equals(KeyframeWithTangents<T> other)
+    => FrameComparisonUtil.AreSameFrame(this.Frame, other.Frame) &&
+       (this.Value?.Equals(other.Value) ??
+        this.Value == null && other.Value == null);
+}
