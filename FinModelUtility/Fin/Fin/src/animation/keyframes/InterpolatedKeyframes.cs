decbfba4723c0b7d397c9da715ed22ba267b7f8b
MeltyPlayer <meltyplayer@gmail.com>
2024-06-26 20:05:55 -0500

    Set up types for storing keyframes.

---
+++ FinModelUtility/Fin/Fin/src/animation/keyframes/InterpolatedKeyframes.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+using fin.animation.interpolation;
+using fin.util.optional;
+
+namespace fin.animation.keyframes;
+
+public interface IInterpolatableKeyframes<TKeyframe, T>
+    : IKeyframes<TKeyframe>,
+      IInterpolatable<T> where TKeyframe : IKeyframe<T>;
+
+public readonly struct IndividualInterpolationConfig<T>() {
+  public int InitialCapacity { get; init; } = 0;
+  public IOptional<T>? DefaultValue { get; init; } = null;
+}
+
+public class InterpolatedKeyframes<TKeyframe, T>(
+    ISharedInterpolationConfig sharedConfig,
+    IKeyframeInterpolator<TKeyframe, T> interpolator,
+    IndividualInterpolationConfig<T> individualConfig = default)
+    : IInterpolatableKeyframes<TKeyframe, T>
+    where TKeyframe : IKeyframe<T> {
+  private readonly List<TKeyframe>
+      impl_ = new(individualConfig.InitialCapacity);
+
+  public IReadOnlyList<TKeyframe> Definitions => this.impl_;
+
+  public void Add(TKeyframe keyframe) => this.impl_.AddKeyframe(keyframe);
+
+  public bool TryGetAtFrame(float frame, out T value) {
+    switch (this.impl_.TryGetPrecedingAndFollowingKeyframes(
+                frame,
+                sharedConfig.Looping,
+                out var precedingKeyframe,
+                out var followingKeyframe)) {
+      case KeyframesUtil.InterpolationDataType.PRECEDING_AND_FOLLOWING:
+        value = interpolator.Interpolate(precedingKeyframe,
+                                         followingKeyframe,
+                                         frame,
+                                         sharedConfig);
+        return true;
+
+      case KeyframesUtil.InterpolationDataType.PRECEDING_ONLY:
+        value = precedingKeyframe.Value;
+        return true;
+
+      default:
+      case KeyframesUtil.InterpolationDataType.NONE:
+        if (individualConfig.DefaultValue?.Try(out value) ?? false) {
+          return true;
+        }
+
+        value = default;
+        return false;
+    }
+  }
+}
---
+++ FinModelUtility/Fin/Fin/src/animation/keyframes/StairStepKeyframes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+using fin.animation.interpolation;
+
+namespace fin.animation.keyframes;
+
+public interface IStairStepKeyframes<T>
+    : IKeyframes<Keyframe<T>>, IInterpolatable<T>;
+
+public class StairStepKeyframes<T>(
+    ISharedInterpolationConfig sharedConfig,
+    IndividualInterpolationConfig<T> individualConfig = default)
+    : IStairStepKeyframes<T> {
+  private readonly List<Keyframe<T>> impl_
+      = new(individualConfig.InitialCapacity);
+
+  public IReadOnlyList<Keyframe<T>> Definitions => this.impl_;
+
+  public void Add(Keyframe<T> keyframe) => this.impl_.AddKeyframe(keyframe);
+
+  public bool TryGetAtFrame(float frame, out T value) {
+    if (this.impl_.TryGetPrecedingKeyframe(frame,
+                                           sharedConfig.Looping,
+                                           out var keyframe,
+                                           out _)) {
+      value = keyframe.Value;
+      return true;
+    }
+
+    if (individualConfig.DefaultValue?.Try(out value) ?? false) {
+      return true;
+    }
+
+    value = default;
+    return false;
+  }
+}
