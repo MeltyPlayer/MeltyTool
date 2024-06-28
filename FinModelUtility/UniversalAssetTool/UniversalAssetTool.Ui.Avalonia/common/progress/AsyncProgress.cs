3d4e68647d25dcf31a51b3b7d9543e65314da532
MeltyPlayer <meltyplayer@gmail.com>
2024-06-28 17:47:51 -0500

    Set up more progress value types as needed.

--- FinModelUtility/UniversalAssetTool/UniversalAssetTool.Ui.Avalonia/common/progress/AsyncProgress.cs
+++ FinModelUtility/UniversalAssetTool/UniversalAssetTool.Ui.Avalonia/common/progress/AsyncProgress.cs
@@ -8,14 +8,17 @@ using uni.ui.avalonia.ViewModels;
 
 namespace uni.ui.avalonia.common.progress;
 
-public class AsyncProgress : ViewModelBase, IAsyncProgress<object> {
+public class AsyncProgress
+    : ViewModelBase, IMutableIndeterminateProgressValue<object> {
   public object? Value { get; private set; }
 
   public void ReportCompletion(object value) {
     this.Value = value;
-    this.OnComplete?.Invoke(this, this.Value);
+    this.OnCompleteValue?.Invoke(this, this.Value);
+    this.OnComplete?.Invoke(this, EventArgs.Empty);
     this.RaisePropertyChanged(nameof(this.Value));
   }
 
-  public event EventHandler<object>? OnComplete;
+  public event EventHandler<object>? OnCompleteValue;
+  public event EventHandler? OnComplete;
 }
--- FinModelUtility/UniversalAssetTool/UniversalAssetTool.Ui.Avalonia/common/progress/ValueFractionProgress.cs
+++ FinModelUtility/UniversalAssetTool/UniversalAssetTool.Ui.Avalonia/common/progress/ValueFractionProgress.cs
@@ -10,7 +10,7 @@ using uni.ui.avalonia.ViewModels;
 namespace uni.ui.avalonia.common.progress;
 
 public class ValueFractionProgress
-    : ViewModelBase, IValueFractionProgress<object> {
+    : ViewModelBase, IMutablePercentageProgressValue<object> {
   public float Progress { get; private set; }
   public object? Value { get; private set; }
 
@@ -30,12 +30,14 @@ public class ValueFractionProgress
     this.Value = value;
 
     this.OnProgressChanged?.Invoke(this, this.Progress);
-    this.OnComplete?.Invoke(this, this.Value);
+    this.OnCompleteValue?.Invoke(this, this.Value);
+    this.OnComplete?.Invoke(this, EventArgs.Empty);
 
     this.RaisePropertyChanged(nameof(this.Progress));
     this.RaisePropertyChanged(nameof(this.Value));
   }
 
   public event EventHandler<float>? OnProgressChanged;
-  public event EventHandler<object>? OnComplete;
+  public event EventHandler<object>? OnCompleteValue;
+  public event EventHandler? OnComplete;
 }
