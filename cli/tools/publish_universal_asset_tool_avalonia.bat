4071ebc706581a651f5ba47164740ebea8971490
MeltyPlayer <meltyplayer@gmail.com>
2024-06-30 14:42:56 -0500

    Set up a new batch script for building the new Avalonia UI.

---
+++ cli/tools/publish_universal_asset_tool_avalonia.bat
@@ -0,0 +1,22 @@
+@echo off 
+setlocal EnableDelayedExpansion
+
+echo This will rebuild Universal Asset Tool via Visual Studio. Are you sure you wish to proceed?
+
+pause
+
+set universalAssetToolBasePath=%~dp0%universal_asset_tool\
+
+echo Deleting old universal asset tool...
+del /q "!universalAssetToolBasePath!*"
+
+echo Building new universal asset tool...
+cd ../../
+cd "FinModelUtility\UniversalAssetTool\UniversalAssetTool.Ui.Avalonia.Desktop\"
+
+dotnet publish -c Release
+
+echo Copying new universal asset tool...
+move "bin\Release\net8.0\win-x64\publish\*" "!universalAssetToolBasePath!"
+
+pause
