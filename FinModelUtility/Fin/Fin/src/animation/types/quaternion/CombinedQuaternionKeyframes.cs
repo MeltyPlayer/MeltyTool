f1a384a2f9bd148e12213018875ac3149d19f7e4
MeltyPlayer <meltyplayer@gmail.com>
2024-07-10 22:44:23 -0500

    Started setting up keyframes types for quaternions.

---
+++ FinModelUtility/Fin/Fin/src/animation/types/quaternion/CombinedQuaternionKeyframes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+using fin.animation.interpolation;
+using fin.animation.keyframes;
+
+namespace fin.animation.types.quaternion;
+
+public class CombinedQuaternionKeyframes<TKeyframe>(
+    ISharedInterpolationConfig sharedConfig,
+    IKeyframeInterpolator<TKeyframe, Quaternion> interpolator,
+    IndividualInterpolationConfig<Quaternion> individualConfig = default)
+    : ICombinedQuaternionKeyframes<TKeyframe>
+    where TKeyframe : IKeyframe<Quaternion> {
+  private readonly InterpolatedKeyframes<TKeyframe, Quaternion> impl_
+      = new(sharedConfig, interpolator, individualConfig);
+
+  public IReadOnlyList<TKeyframe> Definitions => this.impl_.Definitions;
+  public bool HasAnyData => this.impl_.HasAnyData;
+
+  public void Add(TKeyframe keyframe) => this.impl_.Add(keyframe);
+
+  public bool TryGetAtFrame(float frame, out Quaternion value)
+    => this.impl_.TryGetAtFrame(frame, out value);
+}
---
+++ FinModelUtility/Fin/Fin/src/animation/types/quaternion/QuaternionInterfaces.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+using fin.animation.keyframes;
+
+namespace fin.animation.types.quaternion;
+
+public interface IQuaternionInterpolatable : IAxesInterpolatable<Quaternion>;
+
+public interface ICombinedQuaternionKeyframes<TKeyframe>
+    : IQuaternionInterpolatable, ICombinedAxesKeyframes<TKeyframe, Quaternion>
+    where TKeyframe : IKeyframe<Quaternion>;
+
+public interface ISeparateQuaternionKeyframes<TKeyframe>
+    : IQuaternionInterpolatable, ISeparateAxesKeyframes<TKeyframe, float, Quaternion>
+    where TKeyframe : IKeyframe<float>;
+
+public interface ISeparateEulerRadiansKeyframes<TKeyframe>
+    : IQuaternionInterpolatable, ISeparateAxesKeyframes<TKeyframe, float, Quaternion>
+    where TKeyframe : IKeyframe<float>;
