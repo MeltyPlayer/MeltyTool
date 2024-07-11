6acd3f59b9d6f8b016977707b65c86a16d88ffab
MeltyPlayer <meltyplayer@gmail.com>
2024-07-10 22:14:50 -0500

    Fixed stupid graphical bug by switching back to always marking matrices as dirty.

--- FinModelUtility/Fin/Fin.Ui/src/rendering/gl/material/BGlMaterialShader.cs
+++ FinModelUtility/Fin/Fin.Ui/src/rendering/gl/material/BGlMaterialShader.cs
@@ -37,73 +37,73 @@ public abstract class BGlMaterialShader<TMaterial> : IGlMaterialShader
       TMaterial material,
       IReadOnlyBoneTransformManager? boneTransformManager,
       IReadOnlyLighting? lighting) {
-      this.model_ = model;
-      this.bonesUsedByVertices_ = model.Skin.BonesUsedByVertices.ToArray();
-      this.Material = material;
-      this.boneTransformManager_ = boneTransformManager;
-      this.lighting_ = lighting;
+    this.model_ = model;
+    this.bonesUsedByVertices_ = model.Skin.BonesUsedByVertices.ToArray();
+    this.Material = material;
+    this.boneTransformManager_ = boneTransformManager;
+    this.lighting_ = lighting;
 
-      var shaderSource = this.GenerateShaderSource(model, material);
-      this.impl_ = GlShaderProgram.FromShaders(
-          shaderSource.VertexShaderSource,
-          shaderSource.FragmentShaderSource);
+    var shaderSource = this.GenerateShaderSource(model, material);
+    this.impl_ = GlShaderProgram.FromShaders(
+        shaderSource.VertexShaderSource,
+        shaderSource.FragmentShaderSource);
 
-      this.modelMatrixUniform_ = this.impl_.GetUniformMat4(
-          GlslConstants.UNIFORM_MODEL_MATRIX_NAME);
-      this.modelViewMatrixUniform_ = this.impl_.GetUniformMat4(
-          GlslConstants.UNIFORM_MODEL_VIEW_MATRIX_NAME);
-      this.projectionMatrixUniform_ = this.impl_.GetUniformMat4(
-          GlslConstants.UNIFORM_PROJECTION_MATRIX_NAME);
+    this.modelMatrixUniform_ = this.impl_.GetUniformMat4(
+        GlslConstants.UNIFORM_MODEL_MATRIX_NAME);
+    this.modelViewMatrixUniform_ = this.impl_.GetUniformMat4(
+        GlslConstants.UNIFORM_MODEL_VIEW_MATRIX_NAME);
+    this.projectionMatrixUniform_ = this.impl_.GetUniformMat4(
+        GlslConstants.UNIFORM_PROJECTION_MATRIX_NAME);
 
-      this.matricesUniform_ = this.impl_.GetUniformMat4s(
-          GlslConstants.UNIFORM_BONE_MATRICES_NAME,
-          1 + model.Skin.BonesUsedByVertices.Count);
-      this.matricesUniform_.SetAndMarkDirty(0, Matrix4x4.Identity);
+    this.matricesUniform_ = this.impl_.GetUniformMat4s(
+        GlslConstants.UNIFORM_BONE_MATRICES_NAME,
+        1 + model.Skin.BonesUsedByVertices.Count);
+    this.matricesUniform_.SetAndMarkDirty(0, Matrix4x4.Identity);
 
-      this.shininessUniform_ = this.impl_.GetUniformFloat(
-          GlslConstants.UNIFORM_SHININESS_NAME);
+    this.shininessUniform_ = this.impl_.GetUniformFloat(
+        GlslConstants.UNIFORM_SHININESS_NAME);
 
-      this.useLightingUniform_ = this.impl_.GetUniformBool(
-          GlslConstants.UNIFORM_USE_LIGHTING_NAME);
+    this.useLightingUniform_ = this.impl_.GetUniformBool(
+        GlslConstants.UNIFORM_USE_LIGHTING_NAME);
 
-      this.cameraPositionUniform_ = this.impl_.GetUniformVec3(
-          GlslConstants.UNIFORM_CAMERA_POSITION_NAME);
+    this.cameraPositionUniform_ = this.impl_.GetUniformVec3(
+        GlslConstants.UNIFORM_CAMERA_POSITION_NAME);
 
-      this.ambientLightColorUniform_ = this.impl_.GetUniformVec4(
-          "ambientLightColor");
+    this.ambientLightColorUniform_ = this.impl_.GetUniformVec4(
+        "ambientLightColor");
 
-      if (lighting != null) {
-        var lights = lighting.Lights;
-        for (var i = 0; i < lights.Count; ++i) {
-          var light = lights[i];
-          this.cachedLightUniformDatas_.AddLast(
-              new CachedLightUniformData(i, light, this.impl_));
-        }
+    if (lighting != null) {
+      var lights = lighting.Lights;
+      for (var i = 0; i < lights.Count; ++i) {
+        var light = lights[i];
+        this.cachedLightUniformDatas_.AddLast(
+            new CachedLightUniformData(i, light, this.impl_));
       }
-
-      this.Setup(material, this.impl_);
     }
+
+    this.Setup(material, this.impl_);
+  }
 
   ~BGlMaterialShader() => this.Dispose();
 
   public void Dispose() {
-      this.ReleaseUnmanagedResources_();
-      GC.SuppressFinalize(this);
-    }
+    this.ReleaseUnmanagedResources_();
+    GC.SuppressFinalize(this);
+  }
 
   private void ReleaseUnmanagedResources_() {
-      this.impl_.Dispose();
+    this.impl_.Dispose();
 
-      if (this.DisposeTextures) {
-        foreach (var cachedTextureUniformData in
-                 this.cachedTextureUniformDatas_) {
-          GlMaterialConstants.DisposeIfNotCommon(
-              cachedTextureUniformData.GlTexture);
-        }
+    if (this.DisposeTextures) {
+      foreach (var cachedTextureUniformData in
+               this.cachedTextureUniformDatas_) {
+        GlMaterialConstants.DisposeIfNotCommon(
+            cachedTextureUniformData.GlTexture);
       }
-
-      this.DisposeInternal();
     }
+
+    this.DisposeInternal();
+  }
 
   protected abstract void DisposeInternal();
 
@@ -126,68 +126,70 @@ public abstract class BGlMaterialShader<TMaterial> : IGlMaterialShader
   public bool DisposeTextures { get; set; } = true;
 
   public void Use() {
-      this.modelMatrixUniform_.SetAndMaybeMarkDirty(GlTransform.ModelMatrix);
-      this.modelViewMatrixUniform_.SetAndMaybeMarkDirty(
-          GlTransform.ModelViewMatrix);
-      this.projectionMatrixUniform_.SetAndMaybeMarkDirty(
-          GlTransform.ProjectionMatrix);
+    // TODO: Figure out how to call SetAndMaybeMarkDirty() without weird
+    // graphical bugs.
+    this.modelMatrixUniform_.SetAndMarkDirty(GlTransform.ModelMatrix);
+    this.modelViewMatrixUniform_.SetAndMarkDirty(
+        GlTransform.ModelViewMatrix);
+    this.projectionMatrixUniform_.SetAndMarkDirty(
+        GlTransform.ProjectionMatrix);
 
-      var cameraPosition = Camera.Instance;
-      var scCamX = cameraPosition.X;
-      var scCamY = cameraPosition.Y;
-      var scCamZ = cameraPosition.Z;
-      this.cameraPositionUniform_.SetAndMaybeMarkDirty(
-          new Vector3(scCamX, scCamY, scCamZ));
+    var cameraPosition = Camera.Instance;
+    var scCamX = cameraPosition.X;
+    var scCamY = cameraPosition.Y;
+    var scCamZ = cameraPosition.Z;
+    this.cameraPositionUniform_.SetAndMaybeMarkDirty(
+        new Vector3(scCamX, scCamY, scCamZ));
 
-      var boneIndex = 1;
-      foreach (var bone in this.bonesUsedByVertices_) {
-        var localToWorldMatrix =
-            this.boneTransformManager_?.GetLocalToWorldMatrix(bone).Impl ??
-            Matrix4x4.Identity;
-        var inverseMatrix =
-            this.boneTransformManager_?.GetInverseBindMatrix(bone).Impl ??
-            Matrix4x4.Identity;
+    var boneIndex = 1;
+    foreach (var bone in this.bonesUsedByVertices_) {
+      var localToWorldMatrix =
+          this.boneTransformManager_?.GetLocalToWorldMatrix(bone).Impl ??
+          Matrix4x4.Identity;
+      var inverseMatrix =
+          this.boneTransformManager_?.GetInverseBindMatrix(bone).Impl ??
+          Matrix4x4.Identity;
 
-        this.matricesUniform_.SetAndMaybeMarkDirty(
-            boneIndex++,
-            inverseMatrix * localToWorldMatrix);
-      }
-
-      this.shininessUniform_.SetAndMaybeMarkDirty(
-          this.Material?.Shininess ?? 0);
-
-      this.PassInLightUniforms_(this.lighting_);
-
-      foreach (var cachedTextureUniformData in
-               this.cachedTextureUniformDatas_) {
-        cachedTextureUniformData.BindTextureAndPassInUniforms();
-      }
-
-      this.PassUniformsAndBindTextures(this.impl_);
-
-      this.impl_.Use();
+      this.matricesUniform_.SetAndMarkDirty(
+          boneIndex++,
+          inverseMatrix * localToWorldMatrix);
     }
+
+    this.shininessUniform_.SetAndMaybeMarkDirty(
+        this.Material?.Shininess ?? 0);
+
+    this.PassInLightUniforms_(this.lighting_);
+
+    foreach (var cachedTextureUniformData in
+             this.cachedTextureUniformDatas_) {
+      cachedTextureUniformData.BindTextureAndPassInUniforms();
+    }
+
+    this.PassUniformsAndBindTextures(this.impl_);
+
+    this.impl_.Use();
+  }
 
   private void PassInLightUniforms_(IReadOnlyLighting? lighting) {
-      var useLighting = this.UseLighting && this.lighting_ != null;
-      this.useLightingUniform_.SetAndMaybeMarkDirty(useLighting);
+    var useLighting = this.UseLighting && this.lighting_ != null;
+    this.useLightingUniform_.SetAndMaybeMarkDirty(useLighting);
 
-      if (!useLighting) {
-        return;
-      }
-
-      var ambientLightStrength = lighting.AmbientLightStrength;
-      var ambientLightColor = lighting.AmbientLightColor;
-      this.ambientLightColorUniform_.SetAndMaybeMarkDirty(new Vector4(
-            ambientLightColor.Rf * ambientLightStrength,
-            ambientLightColor.Gf * ambientLightStrength,
-            ambientLightColor.Bf * ambientLightStrength,
-            ambientLightColor.Af * ambientLightStrength));
-
-      foreach (var cachedLightUniformData in this.cachedLightUniformDatas_) {
-        cachedLightUniformData.PassInUniforms();
-      }
+    if (!useLighting) {
+      return;
     }
+
+    var ambientLightStrength = lighting.AmbientLightStrength;
+    var ambientLightColor = lighting.AmbientLightColor;
+    this.ambientLightColorUniform_.SetAndMaybeMarkDirty(new Vector4(
+          ambientLightColor.Rf * ambientLightStrength,
+          ambientLightColor.Gf * ambientLightStrength,
+          ambientLightColor.Bf * ambientLightStrength,
+          ambientLightColor.Af * ambientLightStrength));
+
+    foreach (var cachedLightUniformData in this.cachedLightUniformDatas_) {
+      cachedLightUniformData.PassInUniforms();
+    }
+  }
 
   protected void SetUpTexture(
       string textureName,
