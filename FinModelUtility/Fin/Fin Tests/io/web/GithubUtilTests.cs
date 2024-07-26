0e056e6ddf921dbf568e85e5cee763ffa9fac52f
MeltyPlayer <meltyplayer@gmail.com>
2024-07-25 20:25:14 -0500

    Set up a helper class for generating prefilled links to the GitHub issue page.

---
+++ FinModelUtility/Fin/Fin Tests/io/web/GithubUtilTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Web;
+
+using NUnit.Framework;
+
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace fin.io.web;
+
+public class GithubUtilTests {
+  [Test]
+  public void TestGetNewIssueUrl() {
+    try {
+      this.SomeMethod1_(0);
+    } catch (Exception e) {
+      var issueUrl = GithubUtil.GetNewIssueUrl(e);
+      var parsedQueryString
+          = HttpUtility.ParseQueryString(issueUrl.Split('?')[1]);
+
+      Assert.AreEqual(
+          """
+          **Stack trace**
+          System.NotImplementedException: Foobar
+              at fin.io.web.GithubUtilTests.SomeMethod2_(System.String message) in //FinModelUtility/Fin/Fin Tests/io/web/GithubUtilTests.cs:line 49
+              at fin.io.web.GithubUtilTests.SomeMethod1_<T>(T value) in //FinModelUtility/Fin/Fin Tests/io/web/GithubUtilTests.cs:line 46
+              at fin.io.web.GithubUtilTests.TestGetNewIssueUrl() in //FinModelUtility/Fin/Fin Tests/io/web/GithubUtilTests.cs:line 14
+          
+          **To Reproduce**
+          Steps to reproduce the behavior:
+          1. Go to '...'
+          2. Click on '....'
+          3. Scroll down to '....'
+          4. See error
+
+          **Additional context**
+          Add any other context about the problem here.
+
+          """,
+          parsedQueryString["description"]);
+      return;
+    }
+
+    Assert.Fail();
+  }
+
+  private void SomeMethod1_<T>(T value) => this.SomeMethod2_("Foobar");
+
+  private void SomeMethod2_(string message)
+    => throw new NotImplementedException(message);
+}
---
+++ FinModelUtility/Fin/Fin/src/io/web/GithubUtil.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using System.Text;
+
+using fin.util.strings;
+
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace fin.io.web;
+
+public static class GithubUtil {
+  public const string GITHUB_URL
+      = "https://github.com/MeltyPlayer/FinModelUtility";
+
+  public const string GITHUB_NEW_ISSUE_URL = $"{GITHUB_URL}/issues/new";
+
+  public const string GITHUB_CHOOSE_NEW_ISSUE_URL
+      = $"{GITHUB_NEW_ISSUE_URL}/choose";
+
+  public static string GetNewIssueUrl(Exception? exception) {
+    if (exception == null) {
+      return GITHUB_CHOOSE_NEW_ISSUE_URL;
+    }
+
+    var description = new StringBuilder();
+    description.AppendLine("**Stack trace**");
+    description.AppendLine(
+        $"{exception.GetType().ToString()}: {exception.Message}");
+
+    var stackTrace = new StackTrace(exception, true);
+    foreach (var frame in stackTrace.GetFrames()) {
+      var method = frame.GetMethod();
+      description.Append($"    at {method.DeclaringType}.{method.Name}");
+
+      if (method.IsGenericMethod) {
+        description.Append('<');
+        var genericArguments = method.GetGenericArguments();
+        for (var i = 0; i < genericArguments.Length; ++i) {
+          if (i > 0) {
+            description.Append(", ");
+          }
+
+          description.Append(genericArguments[i].Name);
+        }
+
+        description.Append('>');
+      }
+
+      {
+        description.Append('(');
+        var parameters = method.GetParameters();
+        for (var i = 0; i < parameters.Length; ++i) {
+          if (i > 0) {
+            description.Append(", ");
+          }
+
+          var parameter = parameters[i];
+          description.Append($"{parameter.ParameterType} {parameter.Name}");
+        }
+
+        description.Append(')');
+      }
+
+      description.Append(" in //")
+                 .Append(frame.GetFileName()
+                              .Replace('\\', '/')
+                              .SubstringAfter("FinModelUtility/"));
+
+      description.AppendLine($":line {frame.GetFileLineNumber()}");
+    }
+
+    description.Append(
+        """
+
+        **To Reproduce**
+        Steps to reproduce the behavior:
+        1. Go to '...'
+        2. Click on '....'
+        3. Scroll down to '....'
+        4. See error
+
+        **Additional context**
+        Add any other context about the problem here.
+
+        """);
+
+    var queryParams = new Dictionary<string, string?> {
+        ["description"] = description.ToString(),
+        ["template"] = "bug_report.md",
+        ["title"] = "[Enhancement] (Enter a description)",
+    };
+
+    var baseIssueUrl
+        = "https://github.com/MeltyPlayer/FinModelUtility/issues/new";
+    return QueryHelpers.AddQueryString(baseIssueUrl, queryParams);
+  }
+}
