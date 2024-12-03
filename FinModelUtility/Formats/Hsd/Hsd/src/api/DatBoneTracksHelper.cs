c55f0e1b64a565c40112e81d4d5d01cb62c77821
MeltyPlayer <meltyplayer@gmail.com>
2024-12-02 23:28:54 -0600

    Modified DatBoneTracksHelper to use capacities.

--- FinModelUtility/Formats/Hsd/Hsd/src/api/DatBoneTracksHelper.cs
+++ FinModelUtility/Formats/Hsd/Hsd/src/api/DatBoneTracksHelper.cs
@@ -9,77 +9,141 @@ public static class DatBoneTracksHelper {
   public static void AddDatKeyframesToBoneTracks(
       IEnumerable<IDatKeyframes> allDatKeyframes,
       IBoneTracks boneTracks) {
-    var translationTrack = boneTracks.UseSeparateTranslationKeyframesWithTangents();
-    var rotationTrack = boneTracks.UseSeparateEulerRadiansKeyframesWithTangents();
-    var scaleTrack = boneTracks.UseSeparateScaleKeyframesWithTangents();
+    IDatKeyframes? translationsX = null;
+    IDatKeyframes? translationsY = null;
+    IDatKeyframes? translationsZ = null;
+
+    IDatKeyframes? rotationsX = null;
+    IDatKeyframes? rotationsY = null;
+    IDatKeyframes? rotationsZ = null;
+
+    IDatKeyframes? scalesX = null;
+    IDatKeyframes? scalesY = null;
+    IDatKeyframes? scalesZ = null;
 
     foreach (var datKeyframes in allDatKeyframes) {
       var jointTrackType = datKeyframes.JointTrackType;
       switch (jointTrackType) {
-        case JointTrackType.HSD_A_J_TRAX:
-        case JointTrackType.HSD_A_J_TRAY:
+        case JointTrackType.HSD_A_J_TRAX: {
+          translationsX = datKeyframes;
+          break;
+        }
+        case JointTrackType.HSD_A_J_TRAY: {
+          translationsY = datKeyframes;
+          break;
+        }
         case JointTrackType.HSD_A_J_TRAZ: {
-          var axis = jointTrackType - JointTrackType.HSD_A_J_TRAX;
-          foreach (var keyframe in datKeyframes.Keyframes) {
-            var frame = keyframe.Frame;
-            var incomingValue = keyframe.IncomingValue;
-            var outgoingValue = keyframe.OutgoingValue;
-            var incomingTangent = keyframe.IncomingTangent;
-            var outgoingTangent = keyframe.OutgoingTangent;
-
-            translationTrack.Axes[axis]
-                            .Add(new KeyframeWithTangents<float>(frame,
-                                   incomingValue,
-                                   outgoingValue,
-                                   incomingTangent,
-                                   outgoingTangent));
-          }
-
+          translationsZ = datKeyframes;
           break;
         }
-        case JointTrackType.HSD_A_J_ROTX:
-        case JointTrackType.HSD_A_J_ROTY:
+        case JointTrackType.HSD_A_J_ROTX: {
+          rotationsX = datKeyframes;
+          break;
+        }
+        case JointTrackType.HSD_A_J_ROTY: {
+          rotationsY = datKeyframes;
+          break;
+        }
         case JointTrackType.HSD_A_J_ROTZ: {
-          var axis = jointTrackType - JointTrackType.HSD_A_J_ROTX;
-          foreach (var keyframe in datKeyframes.Keyframes) {
-            var frame = keyframe.Frame;
-            var incomingValue = keyframe.IncomingValue;
-            var outgoingValue = keyframe.OutgoingValue;
-            var incomingTangent = keyframe.IncomingTangent;
-            var outgoingTangent = keyframe.OutgoingTangent;
-
-            rotationTrack.Axes[axis]
-                         .Add(new KeyframeWithTangents<float>(frame,
-                                incomingValue,
-                                outgoingValue,
-                                incomingTangent,
-                                outgoingTangent));
-          }
-
+          rotationsZ = datKeyframes;
           break;
         }
-        case JointTrackType.HSD_A_J_SCAX:
-        case JointTrackType.HSD_A_J_SCAY:
+        case JointTrackType.HSD_A_J_SCAX: {
+          scalesX = datKeyframes;
+          break;
+        }
+        case JointTrackType.HSD_A_J_SCAY: {
+          scalesY = datKeyframes;
+          break;
+        }
         case JointTrackType.HSD_A_J_SCAZ: {
-          var axis = jointTrackType - JointTrackType.HSD_A_J_SCAX;
-          foreach (var keyframe in datKeyframes.Keyframes) {
-            var frame = keyframe.Frame;
-            var incomingValue = keyframe.IncomingValue;
-            var outgoingValue = keyframe.OutgoingValue;
-            var incomingTangent = keyframe.IncomingTangent;
-            var outgoingTangent = keyframe.OutgoingTangent;
+          scalesZ = datKeyframes;
+          break;
+        }
+      }
+    }
 
-            scaleTrack.Axes[axis]
-                      .Add(new KeyframeWithTangents<float>(
-                               frame,
+    var translationTrack
+        = boneTracks.UseSeparateTranslationKeyframesWithTangents(
+            translationsX?.Keyframes.Count ?? 0,
+            translationsY?.Keyframes.Count ?? 0,
+            translationsZ?.Keyframes.Count ?? 0);
+    Span<IDatKeyframes?> translationAxes =
+        [translationsX, translationsY, translationsZ];
+    for (var i = 0; i < translationAxes.Length; ++i) {
+      var translationAxis = translationAxes[i];
+      if (translationAxis == null) {
+        continue;
+      }
+
+      foreach (var keyframe in translationAxis.Keyframes) {
+        var frame = keyframe.Frame;
+        var incomingValue = keyframe.IncomingValue;
+        var outgoingValue = keyframe.OutgoingValue;
+        var incomingTangent = keyframe.IncomingTangent;
+        var outgoingTangent = keyframe.OutgoingTangent;
+
+        translationTrack.Axes[i]
+                        .Add(new KeyframeWithTangents<float>(frame,
                                incomingValue,
                                outgoingValue,
                                incomingTangent,
                                outgoingTangent));
-          }
+      }
+    }
 
-          break;
-        }
+    var rotationTrack
+        = boneTracks.UseSeparateEulerRadiansKeyframesWithTangents(
+            rotationsX?.Keyframes.Count ?? 0,
+            rotationsY?.Keyframes.Count ?? 0,
+            rotationsZ?.Keyframes.Count ?? 0);
+    Span<IDatKeyframes?> rotationAxes = [rotationsX, rotationsY, rotationsZ];
+    for (var i = 0; i < rotationAxes.Length; ++i) {
+      var rotationAxis = rotationAxes[i];
+      if (rotationAxis == null) {
+        continue;
+      }
+
+      foreach (var keyframe in rotationAxis.Keyframes) {
+        var frame = keyframe.Frame;
+        var incomingValue = keyframe.IncomingValue;
+        var outgoingValue = keyframe.OutgoingValue;
+        var incomingTangent = keyframe.IncomingTangent;
+        var outgoingTangent = keyframe.OutgoingTangent;
+
+        rotationTrack.Axes[i]
+                     .Add(new KeyframeWithTangents<float>(frame,
+                            incomingValue,
+                            outgoingValue,
+                            incomingTangent,
+                            outgoingTangent));
+      }
+    }
+
+    var scaleTrack = boneTracks.UseSeparateScaleKeyframesWithTangents(
+        scalesX?.Keyframes.Count ?? 0,
+        scalesY?.Keyframes.Count ?? 0,
+        scalesZ?.Keyframes.Count ?? 0);
+    Span<IDatKeyframes?> scaleAxes = [scalesX, scalesX, scalesZ];
+    for (var i = 0; i < scaleAxes.Length; ++i) {
+      var scaleAxis = scaleAxes[i];
+      if (scaleAxis == null) {
+        continue;
+      }
+
+      foreach (var keyframe in scaleAxis.Keyframes) {
+        var frame = keyframe.Frame;
+        var incomingValue = keyframe.IncomingValue;
+        var outgoingValue = keyframe.OutgoingValue;
+        var incomingTangent = keyframe.IncomingTangent;
+        var outgoingTangent = keyframe.OutgoingTangent;
+
+        scaleTrack.Axes[i]
+                  .Add(new KeyframeWithTangents<float>(frame,
+                         incomingValue,
+                         outgoingValue,
+                         incomingTangent,
+                         outgoingTangent));
       }
     }
   }
