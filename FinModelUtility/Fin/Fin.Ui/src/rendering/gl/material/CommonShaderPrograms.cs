18fa051dd9d5850bd750eebdbb41770a699456b4
MeltyPlayer <meltyplayer@gmail.com>
2024-12-11 02:37:11 -0600

    Fixed TEXTURELESS_SHADER_PROGRAM to work with the Angle renderer.

--- FinModelUtility/Fin/Fin.Ui/src/rendering/gl/material/CommonShaderPrograms.cs
+++ FinModelUtility/Fin/Fin.Ui/src/rendering/gl/material/CommonShaderPrograms.cs
@@ -1,34 +1,40 @@
-﻿namespace fin.ui.rendering.gl.material;
+﻿using fin.shaders.glsl;
+
+namespace fin.ui.rendering.gl.material;
 
 public static class CommonShaderPrograms {
   private static GlShaderProgram? texturelessShaderProgram_;
 
   public static GlShaderProgram TEXTURELESS_SHADER_PROGRAM {
     get {
-        if (texturelessShaderProgram_ == null) {
-          texturelessShaderProgram_ =
-              GlShaderProgram.FromShaders(@"
-#version 120
+      if (texturelessShaderProgram_ == null) {
+        texturelessShaderProgram_ =
+            GlShaderProgram.FromShaders(
+                """
+                #version 120
 
-varying vec4 vertexColor;
+                varying vec4 vertexColor;
 
-void main() {
-    gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex; 
-    vertexColor = gl_Color;
-}",
-                                          @"
-#version 130 
+                void main() {
+                    gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex; 
+                    vertexColor = gl_Color;
+                }
+                """,
+                $$"""
+                #version 130
+                {{GlslConstants.FLOAT_PRECISION}}
 
-out vec4 fragColor;
+                out vec4 fragColor;
 
-in vec4 vertexColor;
+                in vec4 vertexColor;
 
-void main() {
-    fragColor = vertexColor;
-}");
-        }
-
-        return texturelessShaderProgram_;
+                void main() {
+                    fragColor = vertexColor;
+                }
+                """);
       }
+
+      return texturelessShaderProgram_;
+    }
   }
 }
