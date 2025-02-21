c5d7eba50312f7760bebbf023c72b9d07fe4386c
MeltyPlayer <meltyplayer@gmail.com>
2025-02-21 13:23:39 -0600

    Added goldens for Super Mario 64 DS.

---
+++ FinModelUtility/Games/SuperMario64Ds/SuperMario64Ds Tests/Sm64dsModelGoldenTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+
+using fin.io;
+using fin.testing;
+using fin.testing.model;
+
+using NUnit.Framework;
+
+using sm64ds.api;
+
+namespace sm64ds;
+
+public class Sm64dsModelGoldenTests
+    : BModelGoldenTests<Sm64dsModelFileBundle, Sm64dsModelImporter> {
+  [Test]
+  [TestCaseSource(nameof(GetGoldenDirectories_))]
+  public void TestExportsGoldenAsExpected(
+      IFileHierarchyDirectory goldenDirectory)
+    => this.AssertGolden(goldenDirectory);
+
+  public override Sm64dsModelFileBundle GetFileBundleFromDirectory(
+      IFileHierarchyDirectory directory) {
+    return new Sm64dsModelFileBundle {
+        GameName = "super_mario_64_ds",
+        BmdFile = directory.FilesWithExtension(".bmd").Single(),
+        BcaFiles = directory.FilesWithExtension(".bca").ToArray()
+    };
+  }
+
+  private static IFileHierarchyDirectory[] GetGoldenDirectories_() {
+    var rootGoldenDirectory
+        = GoldenAssert
+            .GetRootGoldensDirectory(Assembly.GetExecutingAssembly());
+    return GoldenAssert.GetGoldenDirectories(rootGoldenDirectory)
+                       .ToArray();
+  }
+}
