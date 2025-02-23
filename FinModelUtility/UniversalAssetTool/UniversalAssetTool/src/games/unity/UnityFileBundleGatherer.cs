91f883eaddd810fc80d6002ab9645b1b3acf478f
MeltyPlayer <meltyplayer@gmail.com>
2025-02-22 19:21:06 -0600

    Set up a placeholder file bundle gatherer for Unity games.

---
+++ FinModelUtility/UniversalAssetTool/UniversalAssetTool/src/games/unity/UnityFileBundleGatherer.cs
@@ -0,0 +1,56 @@
+﻿using fin.data.queues;
+using fin.io;
+using fin.io.bundles;
+using fin.util.linq;
+using fin.util.progress;
+
+using uni.platforms.desktop;
+
+namespace uni.games.unity;
+
+public class UnityFileBundleGatherer : IAnnotatedFileBundleGatherer {
+  public void GatherFileBundles(
+      IFileBundleOrganizer organizer,
+      IMutablePercentageProgress mutablePercentageProgress) {
+    var knownUnityGames = new string[] {
+        "DoronkoWanko",
+        "HyperBowl",
+        "LittleKittyBigCity",
+        "LogiartGrimoire",
+        "Yellow Taxi Goes Vroom",
+    };
+
+    foreach (var knownUnityGame in knownUnityGames) {
+      if (!SteamUtils.TryGetGameDirectory(knownUnityGame,
+                                          out var steamDirectory)) {
+        return;
+      }
+
+      if (!TryToFindDataDirectory_(steamDirectory,
+                                   out var dataDirectory)) {
+        return;
+      }
+
+      // TODO: Support Unity models
+    }
+  }
+
+  private static bool TryToFindDataDirectory_(
+      IReadOnlyTreeDirectory steamDirectory,
+      out IReadOnlyTreeDirectory dataDirectory) {
+    var directoryQueue = new FinQueue<IReadOnlyTreeDirectory>(steamDirectory);
+    while (directoryQueue.TryDequeue(out var currentDirectory)) {
+      var currentSubdirs = currentDirectory.GetExistingSubdirs();
+      if (currentDirectory.TryToGetExistingFile("UnityPlayer.dll",
+                                                out var unityPlayerDll)) {
+        return currentSubdirs.TryGetFirst(d => d.Name.EndsWith("_Data"),
+                                          out dataDirectory);
+      }
+
+      directoryQueue.Enqueue(currentSubdirs);
+    }
+
+    dataDirectory = null!;
+    return false;
+  }
+}
