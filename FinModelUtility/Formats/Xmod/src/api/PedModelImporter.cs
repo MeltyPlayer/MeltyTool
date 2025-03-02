9f2e4f25a1525973314a5b1734f514a35b766add
MeltyPlayer <mastergamepro@hotmail.com>
2025-03-01 21:52:09 -0600

    Started working on Ped importer.

--- FinModelUtility/Formats/Xmod/src/api/PedModelImporter.cs
+++ FinModelUtility/Formats/Xmod/src/api/PedModelImporter.cs
@@ -1,11 +1,25 @@
-﻿using fin.model;
+﻿using fin.io;
+using fin.model;
+using fin.model.impl;
 using fin.model.io.importers;
+using fin.util.sets;
+
+using xmod.schema.ped;
 
 
 namespace xmod.api;
 
 public class PedModelImporter : IModelImporter<PedModelFileBundle> {
   public IModel Import(PedModelFileBundle modelFileBundle) {
-    throw new NotImplementedException();
+    var pedFile = modelFileBundle.PedFile;
+    var ped = pedFile.ReadNewFromText<Ped>();
+
+    var files = pedFile.AsFileSet();
+    var finModel = new ModelImpl {
+        FileBundle = modelFileBundle,
+        Files = files
+    };
+
+    return finModel;
   }
 }
