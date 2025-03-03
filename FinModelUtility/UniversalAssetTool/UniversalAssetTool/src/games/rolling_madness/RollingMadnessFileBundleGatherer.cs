1f22912263a10d900f70aef33d0f387b2aa51bd4
MeltyPlayer <meltyplayer@gmail.com>
2025-03-03 15:57:42 -0600

    Create RollingMadnessFileBundleGatherer.cs

---
+++ FinModelUtility/UniversalAssetTool/UniversalAssetTool/src/games/rolling_madness/RollingMadnessFileBundleGatherer.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Compression;
+
+using fin.common;
+using fin.io;
+using fin.io.bundles;
+using fin.util.progress;
+
+using gm.api;
+
+using Melville.SharpFont;
+
+using pmdc.api;
+
+namespace uni.games.rolling_madness;
+
+public class RollingMadnessFileBundleGatherer : IAnnotatedFileBundleGatherer {
+  public void GatherFileBundles(
+      IFileBundleOrganizer organizer,
+      IMutablePercentageProgress mutablePercentageProgress) {
+    if (!DirectoryConstants.ROMS_DIRECTORY.TryToGetExistingSubdir(
+            Path.Join("rolling_madness", ExtractorUtil.PREREQS),
+            out var rmDir)) {
+      return;
+    }
+
+    var fileHierarchy
+        = ExtractorUtil.GetFileHierarchy("rolling_madness", rmDir);
+
+    // TODO: Ase files probably use zlib compression, but how???
+  }
+}
