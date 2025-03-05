438b4944cee2806d3a2a79447c1fa607d44910b5
MeltyPlayer <meltyplayer@gmail.com>
2025-03-04 20:23:44 -0600

    Set up goldens for Celeste 64.

---
+++ FinModelUtility/Games/Celeste64/Celeste64 Tests/Celeste64MapModelGoldenTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+using Celeste64.api;
+
+using fin.io;
+using fin.testing.model;
+using fin.testing;
+
+namespace ttyd;
+
+public class Celeste64MapModelGoldenTests
+    : BModelGoldenTests<Celeste64MapModelFileBundle,
+        Celeste64MapModelImporter> {
+  [Test]
+  [TestCaseSource(nameof(GetGoldenDirectories_))]
+  public void TestExportsGoldenAsExpected(
+      IFileHierarchyDirectory goldenDirectory)
+    => this.AssertGolden(goldenDirectory);
+
+  public override Celeste64MapModelFileBundle GetFileBundleFromDirectory(
+      IFileHierarchyDirectory directory)
+    => new() {
+        MapFile = directory.FilesWithExtension(".map").Single(),
+        TextureDirectory = directory
+    };
+
+  private static IFileHierarchyDirectory[] GetGoldenDirectories_()
+    => GoldenAssert
+       .GetGoldenDirectories(
+           GoldenAssert
+               .GetRootGoldensDirectory(Assembly.GetExecutingAssembly()))
+       .ToArray();
+}
---
+++ FinModelUtility/Games/Celeste64/Celeste64 Tests/goldens/1/output/__TB_empty.fragment.glsl
@@ -0,0 +1,6 @@
+#version 310 es
+precision highp float;
+
+void main() {
+  discard;
+}
