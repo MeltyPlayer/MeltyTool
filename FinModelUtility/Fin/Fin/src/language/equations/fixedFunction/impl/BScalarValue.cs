485d9faefc4e529a86cc342814a9d523964a3db2
MeltyPlayer <meltyplayer@gmail.com>
2025-03-15 16:03:59 -0500

    Simplified fixed function graphs by flattening terms/expressions with a single factor/term.

--- FinModelUtility/Fin/Fin/src/language/equations/fixedFunction/impl/BScalarValue.cs
+++ FinModelUtility/Fin/Fin/src/language/equations/fixedFunction/impl/BScalarValue.cs
@@ -18,6 +18,10 @@ public abstract class BScalarValue : IScalarValue {
       return ScalarConstant.ZERO;
     }
 
+    if (nonZeroTerms.Length == 1) {
+      return nonZeroTerms[0];
+    }
+
     return new ScalarExpression(nonZeroTerms);
   }
 
@@ -57,6 +61,10 @@ public abstract class BScalarValue : IScalarValue {
 
     if (nonOneDenominators.Length == 0) {
       nonOneDenominators = null;
+    }
+    
+    if (nonOneNumerators.Length == 1 && nonOneDenominators == null) {
+      return nonOneNumerators[0];
     }
 
     return new ScalarTerm(nonOneNumerators, nonOneDenominators);
