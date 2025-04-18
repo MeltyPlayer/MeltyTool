230e128b668ebb79dbf2321b97713f46a54c5607
MeltyPlayer <mastergamepro@hotmail.com>
2025-04-18 00:44:55 -0500

    Whoops, left an extra debug line in.

--- FinModelUtility/Libraries/Grezzo/Grezzo/src/material/CmbFixedFunctionMaterial.cs
+++ FinModelUtility/Libraries/Grezzo/Grezzo/src/material/CmbFixedFunctionMaterial.cs
@@ -129,10 +129,6 @@ public class CmbFixedFunctionMaterial {
                   finTexture.LodBias = texMapper.lodBias;
                   finTexture.MinLod = texMapper.minLodBias;
                   finTexture.UvIndex = cmbTexCoord.coordinateIndex;
-
-                  if (finTexture.UvIndex == 1) {
-                    ;
-                  }
                   finTexture.BorderColor = texMapper.BorderColor;
 
                   finTexture.UvType =
