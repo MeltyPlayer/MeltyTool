194356c553b05734b06490d723e14a97dffd3e87
MeltyPlayer <meltyplayer@gmail.com>
2025-04-25 15:08:27 -0500

    Added more SM64DS tests.

---
+++ FinModelUtility/Games/SuperMario64Ds/SuperMario64Ds Tests/goldens/hatena_karabox/output/hatena_mark.fragment.glsl
@@ -0,0 +1,26 @@
+#version 310 es
+precision highp float;
+
+
+struct Texture {
+  sampler2D sampler;
+  mat3x2 transform2d;
+};
+
+uniform Texture texture0;
+
+in vec2 uv0;
+
+out vec4 fragColor;
+
+void main() {
+  vec3 colorComponent = vec3(0.96862745285)*texture(texture0.sampler, texture0.transform2d * vec3((uv0).x, (uv0).y, 1)).rgb;
+
+  float alphaComponent = texture(texture0.sampler, texture0.transform2d * vec3((uv0).x, (uv0).y, 1)).a;
+
+  fragColor = vec4(colorComponent, 1);
+
+  if (!(alphaComponent > 0.95)) {
+    discard;
+  }
+}
