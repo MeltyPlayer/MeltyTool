b66efcb7fb1c9663112b5415739be849a81bf075
MeltyPlayer <meltyplayer@gmail.com>
2025-05-03 19:09:00 -0500

    Updated goldens.

---
+++ FinModelUtility/Libraries/Sysdolphin/Sysdolphin Tests/goldens/super_smash_bros_melee/TyBField/output/0000BB68 (2).fragment.glsl
@@ -0,0 +1,16 @@
+#version 310 es
+precision highp float;
+
+out vec4 fragColor;
+
+void main() {
+  vec3 colorComponent = vec3(1.0,0.549019634724,0.0);
+
+  float alphaComponent = 1.0;
+
+  fragColor = vec4(colorComponent, alphaComponent);
+
+  if (!(alphaComponent > 0.0)) {
+    discard;
+  }
+}
