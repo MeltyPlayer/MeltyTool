2885185353ae9e5245026afaa426dc5f5c8582a2
MeltyPlayer <mastergamepro@hotmail.com>
2025-08-22 23:19:31 -0500

    Added a method to TMEM for telling if a color format/bits per texel pair are valid.

--- FinModelUtility/Libraries/F3dzex2/F3dzex2/image/TmemUtil.cs
+++ FinModelUtility/Libraries/F3dzex2/F3dzex2/image/TmemUtil.cs
@@ -19,4 +19,19 @@ public static class TmemUtil {
         ParseCoordAxis((ushort) fixedPointCoordAxes.ExtractFromRight(12, 12)),
         ParseCoordAxis((ushort) fixedPointCoordAxes.ExtractFromRight(0, 12))
     );
+
+  public static bool AreColorFormatAndBitsPerTexelValid(
+      N64ColorFormat colorFormat,
+      BitsPerTexel bitsPerTexel)
+    => (colorFormat, bitsPerTexel) switch {
+        (N64ColorFormat.RGBA, BitsPerTexel._16BPT) => true,
+        (N64ColorFormat.RGBA, BitsPerTexel._32BPT) => true,
+        (N64ColorFormat.L, BitsPerTexel._4BPT)     => true,
+        (N64ColorFormat.L, BitsPerTexel._8BPT)     => true,
+        (N64ColorFormat.LA, BitsPerTexel._8BPT)    => true,
+        (N64ColorFormat.LA, BitsPerTexel._16BPT)   => true,
+        (N64ColorFormat.CI, BitsPerTexel._4BPT)    => true,
+        (N64ColorFormat.CI, BitsPerTexel._8BPT)    => true,
+        _                                          => false,
+    };
 }
