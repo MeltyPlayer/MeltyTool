fd116e9b26eff20823c0d3873551571f65f78b35
MeltyPlayer <meltyplayer@gmail.com>
2025-08-24 23:02:28 -0500

    Set up a new project for common Avalonia stuff.

--- FinModelUtility/MarioArtistTool/MarioArtistTool.Desktop/Program.cs
+++ FinModelUtility/MarioArtistTool/MarioArtistTool.Desktop/Program.cs
@@ -1,23 +1,46 @@
 ﻿using System;
 
 using Avalonia;
+using Avalonia.OpenGL;
+using Avalonia.ReactiveUI;
+using Avalonia.Win32;
 
-namespace MarioArtistTool.Desktop;
+using fin.services;
 
-class Program
-{
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
-    [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+using MarioArtistTool;
 
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
+using uni.cli;
 
+namespace uni.ui.avalonia.desktop;
+
+class Program {
+  // Initialization code. Don't use any Avalonia, third-party APIs or any
+  // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+  // yet and stuff might break.
+  [STAThread]
+  public static void Main(string[] args) {
+    Cli.Run(args,
+            () => {
+              try {
+                BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+              } catch (Exception e) {
+                ExceptionService.HandleException(e, null);
+              }
+            });
+  }
+
+  // Avalonia configuration, don't remove; also used by visual designer.
+  public static AppBuilder BuildAvaloniaApp()
+    => AppBuilder.Configure<App>()
+                 .UsePlatformDetect()
+                 .With(new AngleOptions {
+                     GlProfiles = [
+                         new GlVersion(GlProfileType.OpenGLES, 3, 1, true)
+                     ],
+                 })
+                 .With(new Win32PlatformOptions {
+                     RenderingMode = [Win32RenderingMode.AngleEgl]
+                 })
+                 .WithInterFont()
+                 .UseReactiveUI();
 }
