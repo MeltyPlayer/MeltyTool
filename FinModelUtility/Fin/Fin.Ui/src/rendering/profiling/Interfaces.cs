4f339b7d07ae0b89e5af600447415db5efb49522
MeltyPlayer <meltyplayer@gmail.com>
2025-09-01 21:59:02 -0500

    Set up a basic implementation of a CPU/GPU profiler, not used yet.

---
+++ FinModelUtility/Fin/Fin.Ui/src/rendering/profiling/Interfaces.cs
@@ -0,0 +1,56 @@
+﻿namespace fin.ui.rendering.profiling;
+
+public enum CpuStat {
+  LOOK_UP_ANIMATION_FRAME,
+  MATRIX_MATH,
+  BIND_TEXTURE,
+  BIND_SHADER,
+  UPDATE_UNIFORM,
+}
+
+public enum GpuStat {
+  BACKGROUND,
+  MODEL,
+  BIND_TEXTURE,
+  BIND_SHADER,
+  UPDATE_UNIFORM,
+}
+
+public interface IProfilerStats {
+  float FrameTotalSeconds { get; }
+  void StartFrame();
+  void StopFrame();
+  
+  float CpuTotalSeconds
+    => this.CpuLookUpAnimationFrameSeconds +
+       this.CpuMatrixMathSeconds +
+       this.CpuBindTextureSeconds +
+       this.CpuBindShaderSeconds +
+       this.CpuUpdateUniformSeconds;
+
+  float CpuLookUpAnimationFrameSeconds { get; }
+  float CpuMatrixMathSeconds { get; }
+  float CpuBindTextureSeconds { get; }
+  float CpuBindShaderSeconds { get; }
+  float CpuUpdateUniformSeconds { get; }
+
+
+  void Start(CpuStat cpuStat);
+  void Stop(CpuStat cpuStat);
+
+  float GpuTotalSeconds
+    => this.GpuBackgroundSeconds +
+       this.GpuModelSecondsSeconds +
+       this.GpuBindTextureSeconds +
+       this.GpuBindShaderSeconds +
+       this.GpuUpdateUniformSeconds;
+
+  float GpuBackgroundSeconds { get; }
+  float GpuModelSecondsSeconds { get; }
+  float GpuBindTextureSeconds { get; }
+  float GpuBindShaderSeconds { get; }
+  float GpuUpdateUniformSeconds { get; }
+
+  void Start(GpuStat gpuStat);
+  void Stop(GpuStat gpuStat);
+}
---
+++ FinModelUtility/Fin/Fin.Ui/src/rendering/profiling/Profiler.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics;
+
+using OpenTK.Graphics.ES30;
+
+namespace fin.ui.rendering.profiling;
+
+public class ProfilerStats : IProfilerStats {
+  private readonly Stopwatch frameStopwatch_ = new();
+
+  private readonly Stopwatch cpuLookUpAnimationFrameStopwatch_ = new();
+  private readonly Stopwatch cpuMatrixMathStopwatch_ = new();
+  private readonly Stopwatch cpuBindTextureStopwatch_ = new();
+  private readonly Stopwatch cpuBindShaderStopwatch_ = new();
+  private readonly Stopwatch cpuUpdateUniformStopwatch_ = new();
+
+  private static int gpuBackgroundQueryId_;
+  private static int gpuModelQueryId_;
+  private static int gpuBindTextureQueryId_;
+  private static int gpuBindShaderQueryId_;
+  private static int gpuUpdateUniformQueryId_;
+
+  public static ProfilerStats INSTANCE { get; } = new();
+
+  public static void InitGl() {
+    gpuBackgroundQueryId_ = GL.GenQuery();
+    gpuModelQueryId_ = GL.GenQuery();
+    gpuBindTextureQueryId_ = GL.GenQuery();
+    gpuBindShaderQueryId_ = GL.GenQuery();
+    gpuUpdateUniformQueryId_ = GL.GenQuery();
+  }
+
+  public float FrameTotalSeconds { get; private set; }
+
+  public void StartFrame() => this.frameStopwatch_.Restart();
+
+  public void StopFrame() => this.FrameTotalSeconds
+      = (float) this.frameStopwatch_.Elapsed.TotalSeconds;
+
+  public float CpuLookUpAnimationFrameSeconds { get; private set; }
+  public float CpuMatrixMathSeconds { get; private set; }
+  public float CpuBindTextureSeconds { get; private set; }
+  public float CpuBindShaderSeconds { get; private set; }
+  public float CpuUpdateUniformSeconds { get; private set; }
+
+  public void Start(CpuStat cpuStat)
+    => (cpuStat switch {
+        CpuStat.LOOK_UP_ANIMATION_FRAME
+            => this.cpuLookUpAnimationFrameStopwatch_,
+        CpuStat.MATRIX_MATH
+            => this.cpuMatrixMathStopwatch_,
+        CpuStat.BIND_SHADER
+            => this.cpuBindShaderStopwatch_,
+        CpuStat.BIND_TEXTURE
+            => this.cpuBindTextureStopwatch_,
+        CpuStat.UPDATE_UNIFORM
+            => this.cpuUpdateUniformStopwatch_,
+        _ => throw new ArgumentOutOfRangeException(
+            nameof(cpuStat),
+            cpuStat,
+            null)
+    }).Restart();
+
+  public void Stop(CpuStat cpuStat) {
+    throw new NotImplementedException();
+  }
+
+  public float GpuBackgroundSeconds { get; private set; }
+  public float GpuModelSecondsSeconds { get; private set; }
+  public float GpuBindTextureSeconds { get; private set; }
+  public float GpuBindShaderSeconds { get; private set; }
+  public float GpuUpdateUniformSeconds { get; private set; }
+
+  public void Start(GpuStat gpuStat) {
+  }
+
+  public void Stop(GpuStat gpuStat) {
+  }
+
+  private static int GetQueryId_(GpuStat gpuStat)
+    => gpuStat switch {
+        GpuStat.BACKGROUND     => gpuBackgroundQueryId_,
+        GpuStat.MODEL          => gpuModelQueryId_,
+        GpuStat.BIND_SHADER    => gpuBindShaderQueryId_,
+        GpuStat.BIND_TEXTURE   => gpuBindTextureQueryId_,
+        GpuStat.UPDATE_UNIFORM => gpuUpdateUniformQueryId_,
+        _ => throw new ArgumentOutOfRangeException(
+            nameof(gpuStat),
+            gpuStat,
+            null)
+    };
+}
