e7236b3c00b3764a769d474a2e92334d1a06bf89
MeltyPlayer <meltyplayer@gmail.com>
2026-08-10 20:35:47 -0500

    Set up a ViewLocator for Avalonia.

---
+++ FinModelUtility/UniversalAssetTool/UniversalAssetTool.Ui.Avalonia/ViewLocator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+
+using fin.util.asserts;
+using fin.util.strings;
+
+namespace uni.ui.avalonia;
+
+// TODO: Associate view model with type via generics instead, get them that way
+// instead of via reflection
+public class ViewLocator : IDataTemplate {
+  public Control Build(object? data) {
+    var type = GetViewType_(data).AssertNonnull();
+    return (Control) Activator.CreateInstance(type)!;
+  }
+
+  public bool Match(object? data) => GetViewType_(data) != null;
+
+  private static Type? GetViewType_(object? data) {
+    var viewModelTypeName = data?.GetType().FullName;
+    if (viewModelTypeName == null) {
+      return null;
+    }
+
+    return viewModelTypeName.TryRemoveEnd("ViewModel", out var viewTypeName)
+        ? Type.GetType(viewTypeName)
+        : null;
+  }
+}
