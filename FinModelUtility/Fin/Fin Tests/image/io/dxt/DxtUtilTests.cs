02239d63ebad2c35cc43c978e303eeced2028c65
MeltyPlayer <meltyplayer@gmail.com>
2026-09-10 17:32:30 -0500

    Set up logic for getting DXT1 metadata about a specific tile from just the sub tile index.

---
+++ FinModelUtility/Fin/Fin Tests/image/io/dxt/DxtUtilTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace fin.image.io.dxt;
+
+internal class DxtUtilTests {
+  [Test]
+  [TestCase(64, 64, 2, 4)]
+  [TestCase(64, 32, 2, 4)]
+  [TestCase(32, 64, 2, 4)]
+  [TestCase(50, 50, 2, 4)]
+  [TestCase(32, 32, 2, 4)]
+  public void TestListsSameSubTilesFrom4LoopsAnd1Loop(
+      int width,
+      int height,
+      int subTileCountInAxis,
+      int subTileSizeInAxis) {
+    var subTilesFrom4Loops
+        = DxtUtil.ListSubtilesWith4Loops(width,
+                                         height,
+                                         subTileCountInAxis,
+                                         subTileSizeInAxis);
+    var subTilesFrom1Loop
+        = DxtUtil.ListSubtilesWith1Loop(width,
+                                        height,
+                                        subTileCountInAxis,
+                                        subTileSizeInAxis);
+
+    CollectionAssert.AreEquivalent(subTilesFrom4Loops, subTilesFrom1Loop);
+  }
+}
---
+++ FinModelUtility/Fin/Fin/src/image/io/dxt/DxtUtil.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace fin.image.io.dxt;
+
+using DxtSubTileTuple = (int imageX, int imageY, int byteOffset);
+
+public static class DxtUtil {
+  public static IEnumerable<DxtSubTileTuple> ListSubtilesWith4Loops(
+      int width,
+      int height,
+      int subTileCountInAxis,
+      int subTileSizeInAxis) {
+    var tileSize = subTileCountInAxis * subTileSizeInAxis;
+
+    var tileXCount = (int) Math.Ceiling(1f * width / tileSize);
+    var tileYCount = (int) Math.Ceiling(1f * height / tileSize);
+
+    var subblockIndex = 0;
+    for (var tileY = 0; tileY < tileYCount; ++tileY) {
+      for (var tileX = 0; tileX < tileXCount; ++tileX) {
+        for (var j = 0; j < subTileCountInAxis; ++j) {
+          for (var i = 0; i < subTileCountInAxis; ++i) {
+            var imageX = tileX * tileSize + i * subTileSizeInAxis;
+            var imageY = tileY * tileSize + j * subTileSizeInAxis;
+
+            var byteOffset = (subblockIndex++) * 8;
+
+            yield return (imageX, imageY, byteOffset);
+          }
+        }
+      }
+    }
+  }
+
+  public static IEnumerable<DxtSubTileTuple> ListSubtilesWith1Loop(
+      int width,
+      int height,
+      int subTileCountInAxis,
+      int subTileSizeInAxis) {
+    var tileSize = subTileCountInAxis * subTileSizeInAxis;
+
+    var tileXCount = (int) Math.Ceiling(1f * width / tileSize);
+    var tileYCount = (int) Math.Ceiling(1f * height / tileSize);
+
+    var subTilesPerTile = subTileCountInAxis * subTileCountInAxis;
+    var subTileCount = tileXCount *
+                       tileYCount *
+                       subTilesPerTile;
+
+    for (var subTileI = 0; subTileI < subTileCount; ++subTileI) {
+      yield return GetSubTileTuple(
+          tileXCount,
+          tileSize,
+          subTileCountInAxis,
+          subTileSizeInAxis,
+          subTilesPerTile,
+          subTileI);
+    }
+  }
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static DxtSubTileTuple GetSubTileTuple(
+      int tileXCount,
+      int tileSize,
+      int subTileCountInAxis,
+      int subTileSizeInAxis,
+      int subTilesPerTile,
+      int subTileI) {
+    var tileI = subTileI / subTilesPerTile;
+    var tileX = tileI % tileXCount;
+    var tileY = tileI / tileXCount;
+
+    var internalSubTileI = subTileI % subTilesPerTile;
+    var subTileX = internalSubTileI % subTileCountInAxis;
+    var subTileY = internalSubTileI / subTileCountInAxis;
+
+    var imageX = tileX * tileSize + subTileX * subTileSizeInAxis;
+    var imageY = tileY * tileSize + subTileY * subTileSizeInAxis;
+
+    var byteOffset = subTileI * 8;
+
+    return (imageX, imageY, byteOffset);
+  }
+}
