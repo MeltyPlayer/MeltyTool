ee98fa89290c6d37ca6d8038e4ed799bb7605309
MeltyPlayer <meltyplayer@gmail.com>
2026-09-11 21:21:23 -0500

    Added tests for Harvest Moon 64.

---
+++ FinModelUtility/Games/HarvestMoon64/HarvestMoon64 Tests/Hm64MapModelGoldenTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+using fin.io;
+using fin.testing;
+using fin.testing.model;
+
+using hm64.api;
+
+namespace hm64;
+
+public sealed class Hm64MapModelGoldenTests
+    : BModelGoldenTests<Hm64MapModelFileBundle, Hm64MapModelImporter> {
+  [Test]
+  [TestCaseSource(nameof(GetGoldenDirectories_))]
+  public async Task TestExportsGoldenAsExpected(
+      IFileHierarchyDirectory goldenDirectory)
+    => await this.AssertGolden(goldenDirectory);
+
+  public override Hm64MapModelFileBundle GetFileBundleFromDirectory(
+      IFileHierarchyDirectory directory)
+    => new(directory.FilesWithExtension(".bin").Single());
+
+  private static IFileHierarchyDirectory[] GetGoldenDirectories_() {
+    var rootGoldenDirectory
+        = GoldenAssert
+            .GetRootGoldensDirectory(Assembly.GetExecutingAssembly());
+    return GoldenAssert.GetGoldenDirectories(rootGoldenDirectory)
+                       .ToArray();
+  }
+}
---
+++ FinModelUtility/Games/HarvestMoon64/HarvestMoon64 Tests/goldens/village1Summer/output/material23.vertex.glsl
@@ -0,0 +1,37 @@
+#version 450
+
+layout (std140, binding = 1) uniform GlobalMatrices {
+  mat4 projectionViewMatrix;
+};
+
+layout (std140, binding = 2) uniform CurrentMatrices {
+  mat4 modelMatrix;
+  mat4 boneMatrices[65];  
+};
+
+uniform vec3 cameraPosition;
+
+layout(location = 0) in vec3 in_Position;
+layout(location = 1) in int in_BoneIds;
+layout(location = 2) in float in_BoneWeights;
+layout(location = 3) in vec2 in_Uv0;
+layout(location = 4) in vec4 in_Color0;
+
+out vec3 vertexPosition;
+out vec2 uv0;
+out vec4 vertexColor0;
+
+void main() {
+  mat4 mvpMatrix = projectionViewMatrix * modelMatrix;
+  mat4 mergedBoneMatrix = boneMatrices[in_BoneIds] * in_BoneWeights;
+
+
+  mat4 vertexModelMatrix = modelMatrix * mergedBoneMatrix;
+  mat4 projectionVertexModelMatrix = mvpMatrix * mergedBoneMatrix;
+
+  gl_Position = projectionVertexModelMatrix * vec4(in_Position, 1);
+
+  vertexPosition = vec3(vertexModelMatrix * vec4(in_Position, 1));
+  uv0 = in_Uv0;
+  vertexColor0 = in_Color0;
+}
