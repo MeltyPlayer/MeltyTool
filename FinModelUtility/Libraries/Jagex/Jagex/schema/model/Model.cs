051f023f25d14c3787fa40526943c9fd12ea1590
MeltyPlayer <meltyplayer@gmail.com>
2026-09-23 20:46:08 -0500

    Actually, found a library that already figured out the relevant details for OSRS and just stole from that.

---
+++ FinModelUtility/Libraries/Jagex/Jagex/schema/model/Model.cs
@@ -0,0 +1,67 @@
+﻿using fin.schema.vector;
+
+using schema.binary;
+using schema.binary.attributes;
+
+namespace jagex.schema.model;
+
+[Flags]
+public enum VertexFlags : byte {
+  HAS_X_OFFSET,
+  HAS_Y_OFFSET,
+  HAS_Z_OFFSET,
+}
+
+/// <summary>
+///   Shamelessly stolen from:
+///   https://github.com/Ddemon26/2006Scape/blob/678f456faefe24aa7bbfe977c47518cd0919708e/2006Scape%20Client/src/main/java/Model.java#L142
+/// </summary>
+[BinarySchema]
+public sealed partial class Model : IBinaryConvertible {
+  public ModelHeader Header { get; } = new();
+
+  [RSequenceLengthSource(nameof(Header.VertexCount))]
+  public VertexFlags[] VertexFlags { get; set; }
+
+  [RSequenceLengthSource(nameof(Header.FaceCount))]
+  public byte[] FacePriorities { get; set; }
+
+  [RIfBoolean(nameof(Header.HasFaceAlphas))]
+  [RSequenceLengthSource(nameof(Header.FaceCount))]
+  public byte[]? FaceAlphas { get; set; }
+
+  [RIfBoolean(nameof(Header.HasVertexLabels))]
+  [RSequenceLengthSource(nameof(Header.FaceCount))]
+  public byte[]? VertexLabels { get; set; }
+
+  [RIfBoolean(nameof(Header.HasFaceLabels))]
+  [RSequenceLengthSource(nameof(Header.FaceCount))]
+  public byte[]? FaceLabels { get; set; }
+
+  [RIfBoolean(nameof(Header.HasVertexSkins))]
+  [RSequenceLengthSource(nameof(Header.VertexCount))]
+  public byte[]? VertexSkins { get; set; }
+
+  [RIfBoolean(nameof(Header.HasFaceTextures))]
+  [RSequenceLengthSource(nameof(Header.FaceCount))]
+  public byte[]? FaceTextures { get; set; }
+
+  [RSequenceLengthSource(nameof(Header.FaceTypeCount))]
+  public byte[] FaceTypes { get; set; }
+
+  [RSequenceLengthSource(nameof(Header.FaceCount))]
+  public ushort[] FaceSkins { get; set; }
+
+  [RSequenceLengthSource(nameof(Header.TexturedTriangleCount))]
+  public Vector2s[] FaceIndices { get; set; }
+
+  [RSequenceLengthSource(nameof(Header.VertexXCount))]
+  public byte[] VertexXs { get; set; }
+
+  [RSequenceLengthSource(nameof(Header.VertexYCount))]
+  public byte[] VertexYs { get; set; }
+
+  [RSequenceLengthSource(nameof(Header.VertexZCount))]
+  public byte[] VertexZs { get; set; }
+
+}
---
+++ FinModelUtility/Libraries/Jagex/Jagex/schema/model/ModelHeader.cs
@@ -0,0 +1,36 @@
+﻿using schema.binary;
+using schema.binary.attributes;
+
+namespace jagex.schema.model;
+
+/// <summary>
+///   Shamelessly stolen from:
+///   https://github.com/Ddemon26/2006Scape/blob/678f456faefe24aa7bbfe977c47518cd0919708e/2006Scape%20Client/src/main/java/Model.java#L35
+/// </summary>
+[BinarySchema]
+public sealed partial class ModelHeader : IBinaryConvertible {
+  public ushort VertexCount { get; set; }
+  public ushort FaceCount { get; set; }
+  public byte TexturedTriangleCount { get; set; }
+
+  [IntegerFormat(SchemaIntegerType.BYTE)]
+  public bool HasFaceLabels { get; set; }
+
+  [IntegerFormat(SchemaIntegerType.BYTE)]
+  public bool HasFaceAlphas { get; set; }
+
+  [IntegerFormat(SchemaIntegerType.BYTE)]
+  public bool HasFaceTextures { get; set; }
+
+  [IntegerFormat(SchemaIntegerType.BYTE)]
+  public bool HasVertexLabels { get; set; }
+
+  [IntegerFormat(SchemaIntegerType.BYTE)]
+  public bool HasVertexSkins { get; set; }
+
+  public ushort VertexXCount { get; set; }
+  public ushort VertexYCount { get; set; }
+  public ushort VertexZCount { get; set; }
+
+  public ushort FaceTypeCount { get; set; }
+}
